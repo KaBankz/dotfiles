29af926661c2b586be5e8fee8215f75aee0abc43
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-08-05 19:55:09 -0400

    🤡 Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Make sure the script is being executed with superuser privileges
+# Make sure the script is not being executed with superuser privileges
 if [ ! $(id -u) -ne 0 ]; then
-  echo "❌ Please run as root"
+  echo "❌ Please do not run as root"
   exit
 fi
 
@@ -15,43 +15,29 @@ XDG_CACHE_HOME="$HOME/.cache"
 XDG_DATA_HOME="$HOME/.local/share"
 XDG_STATE_HOME="$HOME/.local/state"
 
-# Create XDG dirs
-createXDGDirs() {
+# Run os specific preconfiguration
+preConfig() {
+  echo "⏳ Pre-configuring..."
+
   echo "⏳ Creating XDG dirs..."
-  mkdir -p $XDG_CONFIG_HOME
-  mkdir -p $XDG_CACHE_HOME
-  mkdir -p $XDG_DATA_HOME
-  mkdir -p $XDG_STATE_HOME
+  mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME $XDG_STATE_HOME
 
   # Make zsh history dir
   # This folder is required for zsh to store history
   mkdir -p $XDG_STATE_HOME/zsh
   echo "✅ Finished creating XDG dirs"
-}
 
-# Run os specific preconfiguration
-preConfig() {
-  echo "⏳ Pre-configuring..."
   case "$os" in
   "Darwin")
     {
       echo "🍎 Identified MacOS"
 
-      if [ ! command -v xcode-select -p ] &>/dev/null; then
-        echo "🙅‍♂️ xcode command line tools are not installed"
-
-        echo "⏳ Installing xcode command line tools"
-        # xcode-select --install
-        echo "✅ Installed xcode command line tools"
-      else
-        echo "✅ xcode command line tools are already installed"
-      fi
-
-      if [ ! command -v brew ] &>/dev/null; then
+      if ! [ -x "$(command -v brew)" ]; then
         echo "🙅‍♂️ Homebrew is not installed"
 
-        echo "⏳ Installing homebrew"
-        # curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+        echo "⏳ Installing homebrew..."
+        # Homebrew will check for xcode developer tools and install them if they are not installed
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         echo "✅ Installed homebrew"
       else
         echo "✅ Homebrew is already installed"
@@ -72,16 +58,32 @@ preConfig() {
   echo "✅ Pre-configuration complete"
 }
 
+updateRepos() {
+  echo "⏳ Updating repositories..."
+  if [ -x "$(command -v pacman)" ]; then
+    sudo pacman -Syu
+  elif [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get update && sudo apt-get upgrade -y
+  elif [ -x "$(command -v brew)" ]; then
+    brew update && brew upgrade
+  else
+    echo "❌ No supported package manager found lmao"
+    exit
+  fi
+  echo "✅ Finished updating repositories"
+}
+
 # Install packages with os specific package manager
 installPackage() {
-  if [ command -v pacman -p ] &>/dev/null; then
-    pacman -S $0
-  elif [ command -v apt -p ] &>/dev/null; then
-    apt install $0
-  elif [ command -v brew -p ] &>/dev/null; then
+  if [ -x "$(command -v pacman)" ]; then
+    sudo pacman -S $0
+  elif [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get install $0
+  elif [ -x "$(command -v brew)" ]; then
     brew install $0
   else
     echo "❌ No supported package manager found"
+    exit
   fi
 }
 
@@ -90,9 +92,9 @@ installPrerequisitePackages() {
   echo "⏳ Installing prerequisite packages..."
   prerequisites=(git stow zsh)
   for package in "${prerequisites[@]}"; do
-    if [ ! command -v $package -p ] &>/dev/null; then
-      echo "⏳ Installing $package"
-      # installPackage $package
+    if ! [ -x "$(command -v $package)" ]; then
+      echo "⏳ Installing $package..."
+      installPackage $package
     else
       echo "✅ $package is already installed"
     fi
@@ -107,10 +109,10 @@ installPackageList() {
   "Darwin")
     {
       echo "🍎 Identified MacOS"
-			# brew bundle --file $HOME/.dotfiles/pkgs/Brewfile
-      
-			## OUTDATED METHOD
-			# ? Not sure if this method works for all package managers
+      # brew bundle --file $HOME/.dotfiles/pkgs/Brewfile
+
+      ## OUTDATED METHOD
+      # ? Not sure if this method works for all package managers
       # xargs installPackage < $HOME/.dotfiles/pkgs/brew.txt
     }
     ;;
@@ -131,48 +133,56 @@ installPackageList() {
 # Clone dotfiles repo
 cloneDotfiles() {
   if [ -d "$HOME/.dotfiles" ]; then
-    echo "✅ Dotfiles repo already cloned"
+    if [ $(pwd) == "$HOME/.dotfiles" ]; then
+      echo "✅ Dotfiles repo already exists"
+    else
+      echo "❌ ~/.dotfiles dir already exists"
+      echo "⏳ Backuping ~/.dotfiles dir..."
+      mv $HOME/.dotfiles $HOME/.dotfiles.bak
+      git clone "https://github.com/KaBankz/dotfiles.git" $HOME/.dotfiles
+    fi
   else
     echo "🙅‍♂️ Dotfiles repo not cloned"
 
-    echo "⏳ Cloning dotfiles repo"
-    # git clone "https://github.com/KaBankz/dotfiles.git" $HOME/.dotfiles
+    echo "⏳ Cloning dotfiles repo..."
+    git clone "https://github.com/KaBankz/dotfiles.git" $HOME/.dotfiles
     echo "✅ Cloned dotfiles repo"
   fi
 }
 
 # Stow all dotfiles
 installDotfiles() {
-  echo "⏳ Stowing dotfiles"
+  echo "⏳ Stowing dotfiles..."
   # stow -Sv -d $HOME/.dotfiles -t $HOME
   echo "✅ Stowed dotfiles"
 }
 
 # Set system configs on MacOS
 setDefaults() {
-	echo "⏳ Setting defaults"
-# Enable linux like window dragging (drag windows from anywhere, not just titlebar) with CTRL+CMD+Drag
-  # defaults write -g NSWindowShouldDragOnGesture -bool true
-	echo "✅ Finished setting defaults"
+  echo "⏳ Setting defaults..."
+  # Enable linux like window dragging (drag windows from anywhere, not just titlebar) with CTRL+CMD+Drag
+  defaults write -g NSWindowShouldDragOnGesture -bool true
+  echo "✅ Finished setting defaults"
 }
 
 # Post install
 postInstall() {
-  echo "⏳ Post-installation"
-  echo "⏳ Changing default shell to zsh"
-  # chsh -s /bin/zsh
-  echo "✅ Changed default shell to zsh"
+  echo "⏳ Post-installation..."
+  if [ ! $SHELL == "/bin/zsh" ]; then
+    echo "🙅‍♂️ Default shell is $SHELL instead of zsh"
+    echo "⏳ Changing default shell to zsh..."
+    sudo chsh -s /bin/zsh
+    echo "✅ Changed default shell to zsh"
+  fi
   if [ $os == "Darwin" ]; then
-    echo "⏳ Setting system defaults"
     setDefaults
-    echo "✅ Set system defaults"
   fi
   echo "✅ Post-installation finished"
 }
 
 echo "⚡️ Starting setup script"
-createXDGDirs
 preConfig
+updateRepos
 installPrerequisitePackages
 cloneDotfiles
 installDotfiles
