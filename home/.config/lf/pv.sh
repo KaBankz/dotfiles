10c892079939506ce68464103efe92d1801b67b6
KaBankz <20971850+KaBankz@users.noreply.github.com>
2023-02-25 14:10:47 -0500

    chore(lf): remove lf configs

--- home/.config/lf/pv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Force bat to use interpolated 8-bit colors
-unset COLORTERM
-# -p/--plain renders plain unstyled text
-# -n/--number renders line numbers
-# --color=always renders colors
-# --terminal-width shrink the size of bat output to fit lf preview
-bat --plain --number --color=always --theme=Dracula --terminal-width $(($2-4)) "$1"
