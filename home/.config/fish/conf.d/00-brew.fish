08f36ce75771ce3d90e2a7c3e9395e9efcfc1d2e
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-04-20 16:19:48 -0400

    chore(fish): hardcode brew env vars

--- home/.config/fish/conf.d/00-brew.fish
+++ home/.config/fish/conf.d/00-brew.fish
@@ -1,19 +1,20 @@
 # load homebrew
 # this is up at the top to make sure it's loaded before anything else
 # as homebrew manages the majority of the used binaries
-# this also only runs if not in a nested shell to avoid duplicated PATH
-if test "$SHLVL" -eq 1
-    eval (/opt/homebrew/bin/brew shellenv)
-end
 
-# TODO: insead of running this script, just hardcode the paths manually
-# below is the output of `brew shellenv`
-# set -gx HOMEBREW_PREFIX /opt/homebrew
-# set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
-# set -gx HOMEBREW_REPOSITORY /opt/homebrew
-# set -q PATH; or set PATH ''
-# set -gx PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
-# set -q MANPATH; or set MANPATH ''
-# set -gx MANPATH /opt/homebrew/share/man $MANPATH
-# set -q INFOPATH; or set INFOPATH ''
-# set -gx INFOPATH /opt/homebrew/share/info $INFOPATH
+# hardcoding homebrew env and path to avoid running `brew shellenv` because
+# they do not properly add to the PATH variable causing duplicated
+# entries in nested shells and other issues (duplicated MANPATH and INFOPATH)
+set -gx HOMEBREW_PREFIX /opt/homebrew
+set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
+set -gx HOMEBREW_REPOSITORY /opt/homebrew
+set -gx MANPATH /opt/homebrew/share/man
+set -gx INFOPATH /opt/homebrew/share/info
+
+fish_add_path -P /opt/homebrew/bin /opt/homebrew/sbin
+
+# old
+# this only runs if not in a nested shell to avoid duplicated PATH
+# if test "$SHLVL" -eq 1
+#     eval (/opt/homebrew/bin/brew shellenv)
+# end
