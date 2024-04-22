17d0c6b00a9021a199f4b6e337212622fe302804
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-04-22 10:10:35 -0400

    fix(fish): show hidden items and hide gitignore items in tree view

--- fish/functions/tree.fish
+++ fish/functions/tree.fish
@@ -1,4 +1,4 @@
 # use eza to display dir as a tree
-function tree --wraps='eza -T' --description 'alias tree=eza -T'
-    eza -T $argv
+function tree --wraps='eza -Ta --git-ignore' --description 'alias tree=eza -Ta --git-ignore'
+    eza -Ta --git-ignore $argv
 end
