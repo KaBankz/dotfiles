a5a581354f4da5512f5306437844d4561847a9ca
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-04-22 00:14:43 -0400

    chore(fish)!: clean up fastfetch

--- fish/functions/neofetch.fish
+++ fish/functions/neofetch.fish
@@ -1,6 +1,4 @@
-# note these custom fastfetch configs are stored in ~/.local/share/fastfetch/presets
-# most of them are taken from the fastfetch repo
 # replace neofetch with the superior fastfetch
-function neofetch --wraps='fastfetch --load-config neofetch.jsonc' --description 'alias neofetch=fastfetch --load-config neofetch.jsonc'
-    fastfetch --load-config neofetch.jsonc $argv
+function neofetch --wraps='fastfetch -c neofetch' --description 'alias neofetch=fastfetch -c neofetch'
+    fastfetch -c neofetch $argv
 end
--- fish/functions/pfetch.fish
+++ fish/functions/pfetch.fish
@@ -1,6 +1,4 @@
-# note these custom fastfetch configs are stored in ~/.local/share/fastfetch/presets
-# most of them are taken from the fastfetch repo
 # replace pfetch with the superior fastfetch
-function pfetch --wraps='fastfetch --load-config pfetch.jsonc' --description 'alias pfetch=fastfetch --load-config pfetch.jsonc'
-    fastfetch --load-config pfetch.jsonc $argv
+function pfetch --wraps='fastfetch -c pfetch' --description 'alias pfetch=fastfetch -c pfetch'
+    fastfetch -c pfetch $argv
 end
