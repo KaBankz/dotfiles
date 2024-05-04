4dc991c3c97de876f47dfc02b171e8f7b3fc8889
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-04 14:26:18 -0400

    feat(fish): brew update & upgrade helper function

---
+++ fish/functions/brewup.fish
@@ -0,0 +1,31 @@
+function brewup --description "Update Homebrew and all installed packages"
+    if command -v brew >/dev/null
+        if brew update >/dev/null
+            if test -n "$(brew outdated)"
+                echo "The following packages are outdated:"
+                brew outdated
+                read -lP "Do you want to upgrade packages? [y/N] " upgrade_choice
+                switch $upgrade_choice
+                    case Y y
+                        echo "Upgrading packages..."
+                        brew upgrade
+                    case N n
+                        echo "Exiting without upgrading."
+                        return 0
+                    case '*'
+                        echo "Invalid choice. Exiting."
+                        return 1
+                end
+            else
+                echo "All packages are up to date."
+                return 0
+            end
+        else
+            echo "Error: Failed to update Homebrew. Exiting."
+            return 1
+        end
+    else
+        echo "Error: Homebrew is not installed. Please install Homebrew first."
+        return 1
+    end
+end
