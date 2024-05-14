16bffac67c5b277526ed9cd088e6b8ded1c5e5a0
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-14 00:15:02 -0400

    feat(fish): expand `runx` capabilities
    Now it logs a message if a command is not found
    
    It also handles `command` prefixed commands

--- fish/functions/runx.fish
+++ fish/functions/runx.fish
@@ -1,7 +1,18 @@
 function runx --description 'Run a command only if it exists'
-    if type -q $argv[1]
+    # handle `command` prefix
+    if test $argv[1] = command
+        if type -q $argv[2]
+            # using eval here because without it, only `command` is executed
+            # instead of the entire command
+            eval "$argv"
+        else
+            echo "[runx] Command not found: $argv[2]"
+            return 1
+        end
+    else if type -q $argv[1]
         $argv
     else
+        echo "[runx] Command not found: $argv[1]"
         return 1
     end
 end
