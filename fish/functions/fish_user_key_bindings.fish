39a9451b99009c6dfa72ec51c3e9e09d698eb90c
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-18 16:01:50 -0400

    feat(fish): add zoxide interactive keybinding

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,12 @@
+# If a script produces output, it should finish by calling
+# `commandline -f repaint` to tell fish that a repaint is in order.
+# ref: https://fishshell.com/docs/current/cmds/bind.html
+
 # show random pokemon with ctrl + k
 # the echo stops the name from being print in the prompt
-# the commandline -f repaint, repaints the prompt after executing the krabby command
 # only bind if krabby is installed
 type -q krabby && bind \ck "echo; krabby random; echo; commandline -f repaint"
+
+# launch zoxide interactive mode with ctrl + f
+# only bind if zoxide is installed
+type -q zoxide && bind \cf "cdi; commandline -f repaint"
