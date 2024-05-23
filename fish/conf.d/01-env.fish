d21b5c43e83fed9090b871bb28af99d7f952253b
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-22 21:13:07 -0400

    chore(fish): remove quotes
    Fish does not need them around variables

--- fish/conf.d/01-env.fish
+++ fish/conf.d/01-env.fish
@@ -1,15 +1,15 @@
 # XDG Base Directory Specification
-set -gx XDG_CONFIG_HOME "$HOME"/.config
-set -gx XDG_CACHE_HOME "$HOME"/.cache
-set -gx XDG_DATA_HOME "$HOME"/.local/share
-set -gx XDG_STATE_HOME "$HOME"/.local/state
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
 
 # set default editors
 set -gx EDITOR nvim
 set -gx VISUAL code --wait
 
 # set ZDOTDIR for zsh subshells
-set -gx ZDOTDIR "$HOME"/.config/zsh
+set -gx ZDOTDIR $HOME/.config/zsh
 
 # set bat as the manpager if it exists
 if type -q bat
@@ -21,7 +21,7 @@ set -gx LESSHISTFILE -
 
 # set bash history file
 # dir must exist else bash will not save history
-set -gx HISTFILE "$XDG_STATE_HOME"/bash/history
+set -gx HISTFILE $XDG_STATE_HOME/bash/history
 
 set -gx MANPATH $MANPATH (path_helper -m)
 
@@ -34,7 +34,7 @@ set -gx HOMEBREW_DISPLAY_INSTALL_TIMES 1
 set -gx MISE_NOT_FOUND_AUTO_INSTALL false
 
 # gnupg
-set -gx GNUPGHOME "$XDG_DATA_HOME"/gnupg
+set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
 # load gpg keys
 set -gx GPG_TTY (tty)
 
@@ -45,35 +45,35 @@ set -gx ANDROID_HOME $HOME/Library/Android/sdk
 set -gx FLUTTER_ROOT (runx mise where flutter)
 
 # gradle
-set -gx GRADLE_USER_HOME "$XDG_DATA_HOME"/gradle
+set -gx GRADLE_USER_HOME $XDG_DATA_HOME/gradle
 
 # disable bun analytics
 set -gx DISABLE_BUN_ANALYTICS 1
 # set bun install path
-set -gx BUN_INSTALL "$XDG_DATA_HOME/bun"
+set -gx BUN_INSTALL $XDG_DATA_HOME/bun
 
 # npm
-set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
-set -gx NODE_REPL_HISTORY "$XDG_DATA_HOME"/node_repl_history
+set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+set -gx NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
 
 # pnpm
-set -gx PNPM_HOME "$XDG_DATA_HOME"/pnpm
+set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
 
 # rust
-set -gx RUSTUP_HOME "$XDG_DATA_HOME"/rustup
-set -gx CARGO_HOME "$XDG_DATA_HOME"/cargo
+set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
+set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 
 # cocoapods
-set -gx CP_HOME_DIR "$XDG_DATA_HOME"/cocoapods
+set -gx CP_HOME_DIR $XDG_DATA_HOME/cocoapods
 
 # docker
-set -gx DOCKER_CONFIG "$XDG_CONFIG_HOME"/docker
+set -gx DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 
 # path
 fish_add_path -P $ANDROID_HOME/cmdline-tools/latest/bin # $ANDROID_HOME/tools is deprecated in favor of cmdline-tools
-fish_add_path -P "$ANDROID_HOME"/platform-tools
-fish_add_path -P "$XDG_DATA_HOME"/yarn/bin
-fish_add_path -P "$PNPM_HOME"
-fish_add_path -P "$BUN_INSTALL"/bin
-fish_add_path -P "$CARGO_HOME"/bin
-fish_add_path -P "$HOME"/.local/bin
+fish_add_path -P $ANDROID_HOME/platform-tools
+fish_add_path -P $XDG_DATA_HOME/yarn/bin
+fish_add_path -P $PNPM_HOME
+fish_add_path -P $BUN_INSTALL/bin
+fish_add_path -P $CARGO_HOME/bin
+fish_add_path -P $HOME/.local/bin
