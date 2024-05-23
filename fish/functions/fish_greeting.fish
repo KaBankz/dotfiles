cf23c3248e7f4e6b89e7383160c7714e14149c0b
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-22 20:56:37 -0400

    fix(fish): only run krabby in truecolor terminals, fastfetch in no color terminals
    damn you Terminal.app stop being trash

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,10 @@
 function fish_greeting
-    # pokemon shell colorscripts cargo package
-    # --info flag prints the pokemon's pokedex entry
-    runx krabby random --info
+    # krabby only works in truecolor terminals
+    if test "$COLORTERM" = truecolor
+        # pokemon shell colorscripts cargo package
+        # --info flag prints the pokemon's pokedex entry
+        runx krabby random --info
+    else
+        runx neofetch
+    end
 end
