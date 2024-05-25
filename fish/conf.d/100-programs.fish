dab232c732b341ab0e00fe5bbbd42b59e8276b63
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-25 17:04:57 -0400

    fix(fish): typos

--- fish/conf.d/100-programs.fish
+++ fish/conf.d/100-programs.fish
@@ -5,10 +5,10 @@ runx fzf --fish | source
 # --cmd cd replaces the cd command with zoxide
 runx zoxide init fish --cmd cd | source
 
-#! must be excuted last to avoid conflicts with other prompt customizations
+#! must be executed last to avoid conflicts with other prompt customizations
 # start starship
 # --print-full-init prints the full init script directly
-# instead of printing a souce command to then print the init script
+# instead of printing a source command to then print the init script
 # this approach provides a faster startup time
 runx starship init fish --print-full-init | source
 
--- fish/functions/multi_cd.fish
+++ fish/functions/multi_cd.fish
@@ -1,4 +1,4 @@
-# auto expand ".." and longer varients to cd .. (or more)
+# auto expand ".." and longer variants to cd .. (or more)
 # used by the `dotdot` abbreviation in `10-abbr.fish`
 function multi_cd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
