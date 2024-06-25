69c04d37324c0cfe178ad73d8f8f337ef1c25827
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-06-25 18:25:57 -0400

    feat(fish): update ls alias to show links, `.` & `..` dirs and hide header

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,4 @@
 # replace ls with eza
-function ls --wraps='eza -lah --group-directories-first --git --icons --group --time-style "+%h %d %H:%M"' --description 'alias ls=eza -lah --group-directories-first --git --icons --group --time-style "+%h %d %H:%M"'
-    runx eza -lah --group-directories-first --git --icons --group --time-style "+%h %d %H:%M" $argv || command ls -lah --color $argv
+function ls --wraps='eza -laa --group-directories-first --git --icons --group --links --time-style "+%h %d %H:%M"' --description 'alias ls=eza -laa --group-directories-first --git --icons --group --links --time-style "+%h %d %H:%M"'
+    runx eza -laa --group-directories-first --git --icons --group --links --time-style "+%h %d %H:%M" $argv || command ls -lah --color $argv
 end
