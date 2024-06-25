14ef976c2fad7acf7ff9d1f3862b2f9e8fb91479
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-06-25 16:25:11 -0400

    feat(fish): update `eza` to show groups and nicer time style

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,4 @@
 # replace ls with eza
-function ls --wraps='eza -lah --group-directories-first --git --icons' --description 'alias ls=eza -lah --group-directories-first --git --icons'
-    runx eza -lah --group-directories-first --git --icons $argv || command ls -lah --color $argv
+function ls --wraps='eza -lah --group-directories-first --git --icons --group --time-style "+%h %d %H:%M"' --description 'alias ls=eza -lah --group-directories-first --git --icons --group --time-style "+%h %d %H:%M"'
+    runx eza -lah --group-directories-first --git --icons --group --time-style "+%h %d %H:%M" $argv || command ls -lah --color $argv
 end
