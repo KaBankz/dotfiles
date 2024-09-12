408e7f79435154644aef2db8e904ac38df256394
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-09-11 22:06:00 -0400

    feat(fish): enable mise autoactivation

--- fish/conf.d/01-env.fish
+++ fish/conf.d/01-env.fish
@@ -36,7 +36,7 @@ set -gx ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX 1
 
 # disable mise command not found (b/c it overrides my brew command not found)
 set -gx MISE_NOT_FOUND_AUTO_INSTALL 0
-set -gx MISE_FISH_AUTO_ACTIVATE 0
+set -gx MISE_FISH_AUTO_ACTIVATE 1
 
 set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
 # load gpg keys
