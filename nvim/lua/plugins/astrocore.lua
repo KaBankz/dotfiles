9001e3dca87d278b4822f43e86bac10b55f99a7d
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-09-11 22:12:26 -0400

    chore(nvim): update extensions and set scrolloff

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -28,6 +28,7 @@ return {
     options = {
       opt = {                  -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
+        scrolloff = 10,        -- sets vim.opt.scrolloff
         number = true,         -- sets vim.opt.number
         spell = true,          -- sets vim.opt.spell
         signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
