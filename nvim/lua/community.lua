8b56167976b35dc6c7419d2f0f63c37fdfcdec63
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-12-02 14:53:02 -0500

    feat(nvim): set theme to GitHub Dark Default
    The best theme

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -15,5 +15,6 @@ return {
   { import = "astrocommunity.utility.hover-nvim" },
   { import = "astrocommunity.scrolling.mini-animate" },
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
   -- import/override with your plugins folder
 }
