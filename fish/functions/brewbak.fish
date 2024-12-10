2cb0fc86aed1dda6813f78eec2db31cc57bb4b65
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-12-10 17:43:30 -0500

    feat(fish)!: convert some functions to bash
    Trying to make my config more portable

--- fish/functions/brewbak.fish
+++
@@ -1,6 +0,0 @@
-# --force overwrites any previous Brewfile
-function brewbak --wraps "brew bundle" --description "Make a Brewfile backup of all installed packages"
-    runx brew bundle dump --force --formula --casks --taps --file ~/.dotfiles/pkgs/Brewfile $argv
-    git -C ~/.dotfiles add ~/.dotfiles/pkgs/Brewfile
-    git -C ~/.dotfiles commit -m "chore(pkgs): update Brewfile"
-end
