44cbde15ee4d356d8c73add263ba9110aea7a60b
KaBankz <20971850+KaBankz@users.noreply.github.com>
2025-05-03 18:45:06 -0400

    chore(fish): reformat ls function

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,8 @@
 function ls --wraps eza --description "eza: a modern replacement for ls"
-    runx eza -laa --group-directories-first --git --icons --group --links --time-style "+%h %e  %Y"\n"%h %e %H:%M" $argv || command ls -lah --color $argv
+    runx eza -laagH \
+        --group-directories-first \
+        --git \
+        --icons \
+        --time-style "+%h %e  %Y"\n"%h %e %H:%M" \
+        $argv || command ls -lah --color $argv
 end
